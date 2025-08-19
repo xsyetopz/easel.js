bcd662f82231a5eec8ce355e5e088d629712c825
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-08-19 16:01:40 +0300

    add more

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-// Math classes
-export { Vector3 } from './math/Vector3.js';
-export { Quaternion } from './math/Quaternion.js';
-export { Euler } from './math/Euler.js';
-export { Matrix4 } from './math/Matrix4.js';
-export { MathUtils } from './math/MathUtils.js';
+// common
+export { Color } from "./common/Color.js";
+export { Clock } from "./common/Clock.js";
 
-// Object classes
-export { Object3D } from './objects/Object3D.js';
+// cameras
+export { Camera } from "./camera/Camera.js";
+export { PerspCamera } from "./camera/PerspCamera.js";
+export { OrthoCamera } from "./camera/OrthoCamera.js";
 
-// Camera classes
-export { BaseCamera } from './camera/BaseCamera.js';
-export { PerspCamera } from './camera/PerspCamera.js';
-export { OrthoCamera } from './camera/OrthoCamera.js';
+// math
+export { Vector3 } from "./math/Vector3.js";
+export { Quaternion } from "./math/Quaternion.js";
+export { Euler } from "./math/Euler.js";
+export { Matrix4 } from "./math/Matrix4.js";
+export { MathUtils } from "./math/MathUtils.js";
+
+// objects
+export { Object3D } from "./objects/Object3D.js";
+
+
