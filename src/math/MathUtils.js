585b0f9e14401933612386fa501f58e65c7a2037
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-08-19 16:05:08 +0300

    division changes

--- src/math/MathUtils.js
+++ src/math/MathUtils.js
@@ -57,7 +57,7 @@ export class MathUtils {
      * @returns {number} eased value
      */
     static easeInOutCubic(t) {
-        return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
+        return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) * 0.5;
     }
 
     /**
